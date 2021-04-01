450f264f6c1739c1f291ba4cd488ac7b46799d6c
jiocloud <4ygyiioloj@cloud-mail.top>
2021-04-01 11:52:39 +0530

    Initial Commit

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using CustomerManagementProject.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CustomerManagementProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private IRepo<Customer> _repo;
+
+        public CustomerController(IRepo<Customer> repo)
+        {
+            _repo = repo;
+        }
+
+        // GET: api/<CustomerController>
+        [HttpGet]
+        public ActionResult<IEnumerable<Customer>> Get()
+        {
+            try
+            {
+                var result = _repo.GetItems();
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return BadRequest($"Error : {e.Message}");
+            }
+            
+        }
+
+        // GET api/<CustomerController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CustomerController>
+        [HttpPost]
+        public ActionResult<Customer> Post([FromBody] Customer value)
+        {
+            try
+            {
+                _repo.AddItem(value);
+                return Ok(value);
+            }
+            catch (Exception e)
+            {
+                return BadRequest($"Error : {e.Message}");
+            }
+        }
+
+        // PUT api/<CustomerController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CustomerController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models/CustomerRepo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerManagementProject.Models
+{
+    public class CustomerRepo : IRepo<Customer>
+    {
+        public List<Customer> customers { get; set; }
+
+        public CustomerRepo()
+        {
+            customers = new List<Customer>()
+            {
+                new Customer() { Id = 1, Name = "Ramesh", Age = 25},
+                new Customer() { Id = 2, Name = "Suresh", Age = 30}
+            };
+
+        }
+
+        public void AddItem(Customer item)
+        {
+            customers.Add(item);
+        }
+
+        public IEnumerable<Customer> GetItems()
+        {
+            return customers;
+        }
+    }
+}
